7149d8982a29d98bce8c8e9248d2ade263f9ca0c
Moyinoluwa Adenuga <dayoh14@gmail.com>
2022-10-20 21:01:50 +0100

    Separated business logic and restructured code

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -3,15 +3,13 @@ import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { createTodo } from '../../dataLayer/todosAccess'
-import { todoBuilder } from '../../businessLogic/todos'
+import { createTodo } from '../../businessLogic/todos'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
     // TODO: Implement creating a new TODO item
-    const todo = todoBuilder(newTodo, event)
-    const createdTodo = await createTodo(todo)
+    const createdTodo = await createTodo(newTodo, event)
 
     return {
       statusCode: 201,
--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -3,21 +3,13 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
-import { getTodoById, updateTodoAttachmentUrl } from '../../dataLayer/todosAccess'
-import { getUploadUrl } from '../../dataLayer/attachmentUtils'
-
-const bucketName = process.env.ATTACHMENT_S3_BUCKET;
+import { getPresignedUrl } from '../../businessLogic/todos'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
     // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    const todo = await getTodoById(todoId)
-    todo.attachmentUrl = `https://${bucketName}.s3.amazonaws.com/${todoId}`
-
-    await updateTodoAttachmentUrl(todo)
-
-    const url = await getUploadUrl(todoId)
+    const url = await getPresignedUrl(todoId)
 
     return {
       statusCode: 200,
